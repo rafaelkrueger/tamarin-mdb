328bdc8f2345b91db503795b1869bd138acc044c
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-08-20 11:57:43 -0300

    failed-code

--- models/Usuario.js
+++ models/Usuario.js
@@ -24,3 +24,38 @@ const userSchema = new Schema({
 const User = mongoose.model('user', userSchema)
 
 module.exports = User
+
+
+/*
+{
+    "_id": {
+        "$oid": "6300f4fcba84b45771d53e83"
+    },
+    "logo": "",
+    "name": "Tamarin Technologies",
+    "password": "Vidanormal01@",
+    "email": "rafaelkrueger@gmail.com",
+    "site": "https://www.google.com/search?q=tamarin-tech%40gmail.com&rlz=1C1GCEA_enBR982BR982&oq=tamarin-tech%40gmail.com&aqs=chrome..69i57j69i58.4448j1j4&sourceid=chrome&ie=UTF-8",
+    "users": ["rafaelkrueger"],
+    "cardapio": {},
+    "pedidos": [{
+        "id":1,
+        "produto": "almondega",
+        "value": "50.00",
+        "description": "amondega para paladares sofisticados com aroma de loro",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLJvZVsfQi36jVEVfVL9RP95PfVpYGxZdQ5rkAgWtkpQ&s",
+        "cliente": {
+            "nome": "osvaldo",
+            "rua": "monte carlo 145",
+            "bairro": "euroville",
+            "horas": "20:06 - 19/08/2022",
+            "pagamento": "dinheiro"
+        }
+    }],
+    "orcamento": [],
+    "message": [],
+    "__v": {
+        "$numberInt": "0"
+    }
+}
+*/
