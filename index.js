5917f4dda8cc9e0af3d6798e78d56d8bec0ceff9
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-08-20 12:05:10 -0300

    failed-code

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ app.post("/set-user", (req,res)=>{
 
 app.post("/get-user", async (req, res)=>{
     let {email, password} = req.body  
+
     User.find({email:email}).then((response)=>{
         res.send(response)
     }).catch((err)=>{
