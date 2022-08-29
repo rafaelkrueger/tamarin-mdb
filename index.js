abcebda1b0a5c940f377b2e4317e4c656b2fd741
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-08-29 10:50:21 -0300

    commiting

--- index.js
+++ index.js
@@ -139,9 +139,8 @@ app.post("/set-categoria", (req,res)=>{
 
 })
 
-app.post("/set-produto",upload.single('image'),(req,res)=>{
-    const {empresa,product,description,category, value} = req.body
-    const {image} = req.file
+app.post("/set-produto",(req,res)=>{
+    const {empresa,product,description,category, value, image} = req.body
     User.updateOne(
         {_id:empresa},
         {$addToSet: { cardapio:{
