e56dd8b1c653da6d181181d4c0257282926d6fff
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-08-29 10:18:44 -0300

    commiting

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.post("/set-categoria", (req,res)=>{
 
 })
 
-app.post("/set-produto",(req,res)=>{
+app.post("/set-produto",upload.single('image'),(req,res)=>{
     const {empresa,product,description,category, value, image} = req.body
     User.updateOne(
         {_id:empresa},
