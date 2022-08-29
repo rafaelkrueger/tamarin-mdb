e4c74458368bc00e987c832335ea658094064fb8
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-08-29 09:07:05 -0300

    uhu

--- index.js
+++ index.js
@@ -138,8 +138,7 @@ app.post("/set-categoria", (req,res)=>{
 })
 
 app.post("/set-produto", upload.single('image'), (req,res)=>{
-    const {empresa,product,description,category, value} = req.body
-    const { image } = req.file
+    const {empresa,product,description,category, value, image} = req.body
     User.updateOne(
         {_id:empresa},
         {$addToSet: { cardapio:{
