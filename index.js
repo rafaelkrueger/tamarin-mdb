f962ab76fb35ad5d41f7e8af7d9859c98607f9fb
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-11-07 11:08:16 -0300

    sss

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+//models
+const Message = require("./models/message");
+const User = require("./models/Usuario");
+//controllers
+const {
+  setUser,
+  getUser,
+  deleteUser,
+  getEmpresa,
+} = require("./controller/user-controller");
 const { setMessage, home, news } = require("./controller/system-controller");
 //connection
 const conn = require("./connection");
@@ -37,6 +47,120 @@ app.set("views", "./views");
 app.get("/", home);
 app.post("/set-message", setMessage);
 app.get("/news", news);
+
+//user access routes
+app.post("/set-user", setUser);
+app.post("/get-user", getUser);
+app.post("/delete-user", deleteUser);
+app.get("/empresa/:id", getEmpresa);
+
+app.post("/set-categoria", (req, res) => {
+  const { empresa, category } = req.body;
+  User.updateOne({ _id: empresa }, { $addToSet: { categorias: category } })
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/delete-categoria", (req, res) => {
+  const { empresa, category } = req.body;
+  User.updateOne({ _id: empresa }, { $pull: { categorias: category } })
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/set-produto", async (req, res) => {
+  try {
+    const { empresa, product, description, category, value } = req.body;
+    const image = req.files.image;
+    const result = await cloudinary.uploader.upload(image.tempFilePath, {
+      folder: "samples",
+      resource_type: "auto",
+    });
+    User.updateOne(
+      { _id: empresa },
+      {
+        $addToSet: {
+          produto: {
+            product: product,
+            description: description,
+            category: category,
+            value: value,
+            image: result.secure_url,
+            public_id: result.public_id,
+          },
+        },
+      }
+    )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// app.patch("/update-produto", async (req, res) => {
+//   const { empresa, product, description, category, value, image } = req.body;
+//   try {
+//     const { empresa, product, description, category, value, image } = req.body;
+//     const result = await cloudinary.uploader.upload(image, {
+//       folder: "samples",
+//       resource_type: "auto",
+//     });
+//     User.updateOne(
+//       { _id: empresa },
+//       {
+//         $set: {
+//           produto: {
+//             product: product,
+//             description: description,
+//             category: category,
+//             value: value,
+//             image: result.secure_url,
+//             public_id: result.public_id,
+//           },
+//         },
+//       }
+//     )
+//       .then((res) => console.log(res))
+//       .catch((err) => console.log(err));
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+app.post("/delete-produto", (req, res) => {
+  const { empresa, nomeProduto } = req.body;
+
+  User.updateOne(
+    { _id: empresa },
+    { $pull: { produto: { product: nomeProduto } } }
+  )
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/delete-pedido", async (req, res) => {
+  const { empresa, id } = req.body;
+  User.updateOne({ _id: empresa }, { $pull: { pedidos: { id: id } } })
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log("Funcionando na porta: " + PORT);
