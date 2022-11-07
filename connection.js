f3757afc1bf65976abf576cf5610075f4765fa64
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-11-07 09:22:31 -0300

    teste

--- connection.js
+++ connection.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-
-
-module.exports = async function conn(){
-  await mongoose.connect("mongodb+srv://rafaelkrueger:Vidanormal01@tamarin.3bbedo7.mongodb.net/?retryWrites=true&w=majority",
-    {useNewUrlParser: true, 
-        useUnifiedTopology: true}).then(()=>{
-        console.log("Banco conectado!")
-    }).catch((err)=>{
-        console.log(err.message)
+module.exports = async function conn() {
+  await mongoose
+    .connect(`${process.env.DB}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-
-} 
+    .then(() => {
+      console.log("Banco conectado!");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
