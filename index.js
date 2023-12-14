c5cb2220e9b192cb35ec3c3def8b3d7b92fd08d6
Rafael Krueger <64592901+rafaelkrueger@users.noreply.github.com>
2023-12-14 15:16:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -165,56 +165,56 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 //system routes
-// app.get("/", home);
-// app.post("/set-message", setMessage);
-// app.get("/news/:search", news);
-// app.get("/peexels/:search", peexels);
-// app.get("/rastreio/:trackCode", correios);
+app.get("/", home);
+app.post("/set-message", setMessage);
+app.get("/news/:search", news);
+app.get("/peexels/:search", peexels);
+app.get("/rastreio/:trackCode", correios);
 
 //user access routes
-// app.get("/empresa/:site", getEmpresa);
-// app.post("/set-user", setUser);
-// app.post("/get-user", getUser);
-// app.get("/all", allUsers);
+app.get("/empresa/:site", getEmpresa);
+app.post("/set-user", setUser);
+app.post("/get-user", getUser);
+app.get("/all", allUsers);
 app.patch("/update-user", updateUser);
-// app.post("/delete-user", deleteUser);
+app.post("/delete-user", deleteUser);
 
 //costumers access rooutes
-// app.post("/find-costumer", findCostumer);
-// app.post("/create-costumer", createCostumer);
-// app.patch("/delete-costumer", deleteCostumer);
-// app.post("/facebook-costumer", facebookCostumer);
-// app.post("/set-wishlist-costumer", setWishlistCostumer);
-// app.put("/remove-wishlist-costumer", removeWishlistCostumer);
+app.post("/find-costumer", findCostumer);
+app.post("/create-costumer", createCostumer);
+app.patch("/delete-costumer", deleteCostumer);
+app.post("/facebook-costumer", facebookCostumer);
+app.post("/set-wishlist-costumer", setWishlistCostumer);
+app.put("/remove-wishlist-costumer", removeWishlistCostumer);
 
 //products handler routes
-// app.post("/set-categoria", setCategoria);
-// app.post("/delete-categoria", deleteCategoria);
+app.post("/set-categoria", setCategoria);
+app.post("/delete-categoria", deleteCategoria);
 app.post("/set-produto", setProduto);
 app.patch("/update-produto", updateProduto);
 app.post("/delete-produto", deleteProduto);
 app.post("/set-produto-comment", setProdutoComment);
 
 //cupoms handler routes
-// app.post("/set-cupom", setCupom);
-// app.get("/get-cupom/:empresa/:code", getCupom);
-// app.patch("/remove-cupom", removeCupom);
+app.post("/set-cupom", setCupom);
+app.get("/get-cupom/:empresa/:code", getCupom);
+app.patch("/remove-cupom", removeCupom);
 
 //website handler routes
-// app.patch("/website-style", setWebsiteAllStyle);
-// app.patch("/website-card-style", setWebsiteCardStyle);
-// app.patch("/website-details-style", setWebsiteDetailsStyle);
+app.patch("/website-style", setWebsiteAllStyle);
+app.patch("/website-card-style", setWebsiteCardStyle);
+app.patch("/website-details-style", setWebsiteDetailsStyle);
 
 //socials handler routes
-// app.post("/socials/remove-instagram-followers", removeInstagramFollowers);
-// app.post("/socials/post-stories-every-morning", postStoriesEveryMorning);
+app.post("/socials/remove-instagram-followers", removeInstagramFollowers);
+app.post("/socials/post-stories-every-morning", postStoriesEveryMorning);
 
 //payment handler routes
-// app.post("/pix", getPix);
-// app.get("/pix-status/:txid", verifyPix);
-// app.post("/boleto", boleto);
-// app.post("/card-payment", cardPayment);
-// app.post("/plan", monthlySubscription);
+app.post("/pix", getPix);
+app.get("/pix-status/:txid", verifyPix);
+app.post("/boleto", boleto);
+app.post("/card-payment", cardPayment);
+app.post("/plan", monthlySubscription);
 
 //pedidos handler routes
 app.patch("/pedido-entregue", patchPedido);
